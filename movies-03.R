5fdd09c4d7d1cb28c8a80f37d41a997c00725fc1
graySquirrel <fritzebner@gmail.com>
2022-04-30 12:05:39 -0400

    recreated Movies examples for ae

---
+++ movies-03.R
@@ -0,0 +1,48 @@
+library(shiny)
+library(tidyverse)
+load("data/movies.rdata")
+
+# Define UI
+ui <- fluidPage(
+  
+  # Sidebar layout with a input and output definitions
+  sidebarLayout(
+    # Inputs: Select variables to plot
+    sidebarPanel(
+      # Select variable for y-axis
+      selectInput(inputId = "y", label = "Y-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                  selected = "audience_score"),
+      # Select variable for x-axis
+      selectInput(inputId = "x", label = "X-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                  selected = "critics_score"),
+      sliderInput(inputId = "alpha",
+                  label = "Alpha:",
+                  min = 0, max = 1,
+                  value = 0.5),
+      radioButtons(inputId = "colorby",
+                   label = "ColorBy",
+                   choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                   selected = "imdb_num_votes",
+                   inline = FALSE)
+      ),
+    # Output: Show scatterplot
+    mainPanel(
+      plotOutput(outputId = "scatterplot")
+    )
+  )
+)
+
+  # Define server function
+  server <- function(input, output) {
+    
+    # Create the scatterplot object the plotOutput function is expecting
+    output$scatterplot <- renderPlot({
+      ggplot(data = movies, aes_string(x = input$x, y = input$y, color = input$colorby)) +
+        geom_point(alpha = input$alpha) 
+    })
+  }
+
+shinyApp(ui = ui, server = server)
+
---
+++ movies-04.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(tidyverse)
+load("data/movies.rdata")
+
+# Define UI
+ui <- fluidPage(
+  
+  # Sidebar layout with a input and output definitions
+  sidebarLayout(
+    # Inputs: Select variables to plot
+    sidebarPanel(
+      # Select variable for y-axis
+      selectInput(inputId = "y", label = "Y-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                  selected = "audience_score"),
+      # Select variable for x-axis
+      selectInput(inputId = "x", label = "X-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                  selected = "critics_score"),
+      sliderInput(inputId = "alpha",
+                  label = "Alpha:",
+                  min = 0, max = 1,
+                  value = 0.5),
+      radioButtons(inputId = "colorby",
+                   label = "ColorBy",
+                   choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+                   selected = "imdb_num_votes",
+                   inline = FALSE),
+      checkboxInput(inputId = "displaydf",
+                    label = "DisplayDF",
+                    value = FALSE,
+                    width = '100%')
+      ),
+    # Output: Show scatterplot
+    mainPanel(
+      plotOutput(outputId = "scatterplot"),
+      
+      conditionalPanel(
+        condition = "input.displaydf == true",
+        DT::dataTableOutput("mytable")
+      )
+      
+    )
+  )
+)
+
+  # Define server function
+  server <- function(input, output) {
+    
+    # Create the scatterplot object the plotOutput function is expecting
+    output$scatterplot <- renderPlot({
+      ggplot(data = movies, aes_string(x = input$x, y = input$y, color = input$colorby)) +
+        geom_point(alpha = input$alpha) 
+    })
+    
+    output$mytable <- DT::renderDataTable({movies})
+
+  }
+
+shinyApp(ui = ui, server = server)
+
